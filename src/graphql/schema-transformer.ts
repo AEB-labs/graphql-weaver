6fb61106cfb2ec726361b0edfda447e7016c3ce7
Jan Melcher <jan.melcher@aeb.com>
2017-05-30 15:35:07 +0200

    Refactor type-renamer into generic schema-transformer

---
+++ src/graphql/schema-transformer.ts
@@ -0,0 +1,320 @@
+import {
+    GraphQLEnumType, GraphQLEnumTypeConfig, GraphQLEnumValueConfigMap, GraphQLFieldConfig,
+    GraphQLFieldConfigArgumentMap, GraphQLFieldConfigMap, GraphQLFieldMap, GraphQLInputFieldConfig,
+    GraphQLInputFieldConfigMap, GraphQLInputObjectType, GraphQLInputObjectTypeConfig, GraphQLInputType,
+    GraphQLInterfaceType, GraphQLInterfaceTypeConfig, GraphQLList, GraphQLNamedType, GraphQLNonNull, GraphQLObjectType,
+    GraphQLObjectTypeConfig, GraphQLOutputType, GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig,
+    GraphQLSchema, GraphQLType, GraphQLUnionType, GraphQLUnionTypeConfig
+} from 'graphql';
+import { isNativeGraphQLType } from './native-types';
+
+type TransformationFunction<TConfig> = (config: TConfig, context: SchemaTransformationContext) => void;
+
+/**
+ * An set of transformation functions that can alter parts of a schema
+ */
+export interface SchemaTransformer {
+    transformScalarType?: TransformationFunction<GraphQLScalarTypeConfig<any, any>>;
+    transformEnumType?: TransformationFunction<GraphQLEnumTypeConfig>;
+    transformInterfaceType?: TransformationFunction<GraphQLInterfaceTypeConfig<any, any>>;
+    transformInputObjectType?: TransformationFunction<GraphQLInputObjectTypeConfig>;
+    transformUnionType?: TransformationFunction<GraphQLUnionTypeConfig<any, any>>;
+    transformObjectType?: TransformationFunction<GraphQLObjectTypeConfig<any, any>>;
+
+    transformField?: TransformationFunction<GraphQLFieldConfig<any, any>>;
+    transformInputField?: TransformationFunction<GraphQLInputFieldConfig>;
+}
+
+export interface SchemaTransformationContext {
+    /**
+     * Finds a type of the new schema that corresponds to the given type in the old schema
+     * @param type
+     */
+    mapType(type: GraphQLType): GraphQLType;
+}
+
+function combineTransformationFunctions<TConfig>(fns: (TransformationFunction<TConfig> | undefined)[]): TransformationFunction<TConfig> | undefined {
+    const definedFns = fns.filter(a => a);
+    if (!definedFns.length) {
+        return undefined;
+    }
+    return (config, context) => {
+        definedFns.forEach(fn => fn!(config, context));
+    };
+}
+
+/**
+ * Combines multiple transformers that into one that executes the transformation functions in the given order
+ */
+export function combineTransformers(...transformers: SchemaTransformer[]): SchemaTransformer {
+    return {
+        transformScalarType: combineTransformationFunctions(transformers.map(t => t.transformScalarType)),
+        transformEnumType: combineTransformationFunctions(transformers.map(t => t.transformEnumType)),
+        transformInterfaceType: combineTransformationFunctions(transformers.map(t => t.transformInterfaceType)),
+        transformInputObjectType: combineTransformationFunctions(transformers.map(t => t.transformInputObjectType)),
+        transformUnionType: combineTransformationFunctions(transformers.map(t => t.transformUnionType)),
+        transformObjectType: combineTransformationFunctions(transformers.map(t => t.transformObjectType)),
+        transformField: combineTransformationFunctions(transformers.map(t => t.transformField)),
+        transformInputField: combineTransformationFunctions(transformers.map(t => t.transformInputField))
+    };
+}
+
+/**
+ * Clones a GraphQLSchema by destructuring it into GraphQL's config objects and executes custom transformers
+ * on these config objects
+ *
+ * @param schema
+ * @param transformers
+ */
+export function transformSchema(schema: GraphQLSchema, transformers: SchemaTransformer) {
+    const transformer = new Transformer(transformers);
+    return transformer.transform(schema);
+}
+
+class Transformer {
+    private typeMap: { [typeName: string]: GraphQLNamedType } = {};
+
+    constructor(private transformers: SchemaTransformer) {
+
+    }
+
+    public transform(schema: GraphQLSchema): GraphQLSchema {
+        const originalTypes = Object.values(schema.getTypeMap());
+        const originalInterfaces = originalTypes.filter(t => t instanceof GraphQLInterfaceType);
+
+        // Dependencies between fields and their are broken up via GraphQL's thunk approach (fields are only requested when
+        // needed, which is after all types have been converted). However, an object's reference to its implemented
+        // interfaces does not support the thunk approach, so we need to make sure they are transformed first
+        originalInterfaces.filter(t => t instanceof GraphQLInterfaceType).forEach(t => this.processType(t));
+        originalTypes.filter(t => !(t instanceof GraphQLInterfaceType)).forEach(t => this.processType(t));
+
+        return new GraphQLSchema({
+            types: Object.values(this.typeMap),
+            directives: schema.getDirectives(), // TODO rename
+            query: this.findNewTypeMaybe(schema.getQueryType())!,
+            mutation: this.findNewTypeMaybe(schema.getMutationType()),
+            subscription: this.findNewTypeMaybe(schema.getSubscriptionType())
+        });
+    }
+
+    /**
+     * Finds the type in the new schema by its name in the old schema
+     * @param name the old type name
+     * @returns {GraphQLNamedType}
+     */
+    private findType(name: string) {
+        if (!(name in this.typeMap)) {
+            throw new Error(`Unexpected reference to type ${name} which has not (yet) been renamed`);
+        }
+        return this.typeMap[name];
+    }
+
+    /**
+     * Maps a type in the old schema to a type in the new schema, supporting list and optional types.
+     */
+    private remapType(type: GraphQLType): GraphQLType {
+        if (type instanceof GraphQLList) {
+            return new GraphQLList(this.remapType(type.ofType));
+        }
+        if (type instanceof GraphQLNonNull) {
+            return new GraphQLNonNull(this.remapType(type.ofType));
+        }
+        if (isNativeGraphQLType(type)) {
+            // do not rename native types but keep the reference to singleton objects like GraphQLString
+            return type;
+        }
+        return this.findType(type.name);
+    }
+
+    /**
+     * Finds the new type corresponding to the old type, or undefined if undefined is given
+     */
+    private findNewTypeMaybe(type: GraphQLObjectType | undefined) {
+        if (!type) {
+            return undefined;
+        }
+        const newType = this.findType(type.name);
+        return <GraphQLObjectType>newType;
+    }
+
+    private get transformationContext(): SchemaTransformationContext {
+        return {
+            mapType: this.remapType
+        };
+    }
+
+    /**
+     * Creates a new type for the given old type and stores it in the type map
+     * @param type
+     */
+    private processType(type: GraphQLNamedType) {
+        if (type.name.substring(0, 2) === '__' || isNativeGraphQLType(type)) {
+            // do not touch native types or introspection types like __Schema
+            return;
+        }
+        this.typeMap[type.name] = this.transformType(type);
+    }
+
+    /**
+     * Creates a new type for the given old type. Interfaces are expected to be already present; fields are resolved lazily
+     */
+    private transformType(type: GraphQLNamedType) {
+        if (type instanceof GraphQLScalarType) {
+            return this.transformScalarType(type);
+        }
+        if (type instanceof GraphQLObjectType) {
+            return this.transformObjectType(type);
+        }
+        if (type instanceof GraphQLInputObjectType) {
+            return this.transformInputObjectType(type);
+        }
+        if (type instanceof GraphQLInterfaceType) {
+            return this.transformInterfaceType(type);
+        }
+        if (type instanceof GraphQLUnionType) {
+            return this.transformUnionType(type);
+        }
+        if (type instanceof GraphQLEnumType) {
+            return this.transformEnumType(type);
+        }
+        throw new Error(`Unsupported type: ${type}`);
+    }
+
+    private transformScalarType(type: GraphQLScalarType) {
+        const config: GraphQLScalarTypeConfig<any, any> = {
+            name: type.name,
+            description: type.description,
+            serialize: type.serialize.bind(type),
+            parseLiteral: type.parseLiteral.bind(type),
+            parseValue: type.parseValue.bind(type)
+        };
+        if (this.transformers.transformScalarType) {
+            this.transformers.transformScalarType(config, this.transformationContext);
+        }
+        return new GraphQLScalarType(config);
+    }
+
+    private transformObjectType(type: GraphQLObjectType) {
+        const config = {
+            name: type.name,
+            description: type.description,
+            fields: () => this.transformFields(type.getFields()),
+            interfaces: type.getInterfaces().map(iface => <GraphQLInterfaceType>this.findType(iface.name))
+        };
+        if (this.transformers.transformObjectType) {
+            this.transformers.transformObjectType(config, this.transformationContext);
+        }
+        return new GraphQLObjectType(config);
+    }
+
+    private transformInterfaceType(type: GraphQLInterfaceType) {
+        const config = {
+            name: type.name,
+            description: type.description,
+            fields: () => this.transformFields(type.getFields()),
+
+            // this is likely not to work, so it should be overwritten later, but it's our best choice. Leaving it null
+            // will cause the schema invariants to fail (either resolveType or isTypeOf needs to be implemented)
+            resolveType: !type.resolveType ? undefined :
+                (value: any, context: any, info: GraphQLResolveInfo) => type.resolveType(value, context, info)
+        };
+        if (this.transformers.transformInterfaceType) {
+            this.transformers.transformInterfaceType(config, this.transformationContext);
+        }
+        return new GraphQLInterfaceType(config);
+    }
+
+    /**
+     * Creates field configs for all provided fields, but with remapped types and argument types. All named
+     * types are sent through the typeResolver with their old name to determine the new type.
+     */
+    private transformFields(originalFields: GraphQLFieldMap<any, any>): GraphQLFieldConfigMap<any, any> {
+        const fields: GraphQLFieldConfigMap<any, any> = {};
+        for (const fieldName in originalFields) {
+            const originalField = originalFields[fieldName];
+            const args: GraphQLFieldConfigArgumentMap = {};
+            for (const arg of originalField.args) {
+                args[arg.name] = {
+                    description: arg.description,
+                    type: <GraphQLInputType>this.remapType(arg.type),
+                    defaultValue: arg.defaultValue
+                };
+            }
+            const fieldConfig = {
+                description: originalField.description,
+                deprecationReason: originalField.deprecationReason,
+                type: <GraphQLOutputType>this.remapType(originalField.type),
+                args: args
+            };
+            if (this.transformers.transformField) {
+                this.transformers.transformField(fieldConfig, this.transformationContext);
+            }
+            fields[fieldName] = fieldConfig;
+        }
+        return fields;
+    }
+
+    private transformInputObjectType(type: GraphQLInputObjectType) {
+        const originalFields = type.getFields();
+
+        const getFields = () => {
+            const fields: GraphQLInputFieldConfigMap = {};
+            for (const fieldName in originalFields) {
+                const originalField = originalFields[fieldName];
+                const fieldConfig = {
+                    description: originalField.description,
+                    defaultValue: originalField.defaultValue,
+                    type: <GraphQLInputType>this.remapType(originalField.type)
+                };
+                if (this.transformers.transformInputField) {
+                    this.transformers.transformInputField(fieldConfig, this.transformationContext);
+                }
+                fields[fieldName] = fieldConfig;
+            }
+            return fields;
+        };
+
+        const config = {
+            name: type.name,
+            description: type.description,
+            fields: getFields
+        };
+        if (this.transformers.transformInputObjectType) {
+            this.transformers.transformInputObjectType(config, this.transformationContext);
+        }
+        return new GraphQLInputObjectType(config);
+    }
+
+    private transformEnumType(type: GraphQLEnumType) {
+        const values: GraphQLEnumValueConfigMap = {};
+        for (const originalValue of type.getValues()) {
+            values[originalValue.name] = {
+                description: originalValue.description,
+                value: originalValue.value,
+                deprecationReason: originalValue.deprecationReason
+            };
+        }
+
+        const config = {
+            name: type.name,
+            description: type.description,
+            values
+        };
+        if (this.transformers.transformEnumType) {
+            this.transformers.transformEnumType(config, this.transformationContext);
+        }
+        return new GraphQLEnumType(config);
+    }
+
+    private transformUnionType(type: GraphQLUnionType) {
+        const config: GraphQLUnionTypeConfig<any, any> = {
+            name: type.name,
+            description: type.description,
+            types: type.getTypes().map(optionType => <GraphQLObjectType>this.remapType(optionType))
+        };
+        if (this.transformers.transformUnionType) {
+            this.transformers.transformUnionType(config, this.transformationContext);
+        }
+        return new GraphQLUnionType(config);
+    }
+}
--- src/graphql/type-renamer.ts
+++ src/graphql/type-renamer.ts
@@ -1,215 +1,28 @@
-import {
-    GraphQLEnumType,
-    GraphQLEnumValueConfigMap,
-    GraphQLFieldConfigArgumentMap,
-    GraphQLFieldConfigMap,
-    GraphQLFieldMap,
-    GraphQLInputFieldConfigMap,
-    GraphQLInputObjectType,
-    GraphQLInputType,
-    GraphQLInterfaceType,
-    GraphQLList,
-    GraphQLNamedType,
-    GraphQLNonNull,
-    GraphQLObjectType,
-    GraphQLOutputType,
-    GraphQLResolveInfo,
-    GraphQLScalarType,
-    GraphQLSchema,
-    GraphQLType,
-    GraphQLUnionType
-} from "graphql";
-import {isNativeGraphQLType} from "./native-types";
+import { GraphQLSchema } from 'graphql';
+import { SchemaTransformer, transformSchema } from './schema-transformer';
 
 /**
  * Creates a new schema that equals the given one but with all names of non-native types transformed by a custom callback
  */
-export function renameTypes(schema: GraphQLSchema, transformer: (typeName: string) => string): GraphQLSchema {
-    const typeMap: { [typeName: string]: GraphQLNamedType } = {};
-    const originalTypes = Object.values(schema.getTypeMap());
-    const originalInterfaces = originalTypes.filter(t => t instanceof GraphQLInterfaceType);
-
-    function findType(name: string) {
-        if (!(name in typeMap)) {
-            throw new Error(`Unexpected reference to type ${name} which has not (yet) been renamed`);
-        }
-        return typeMap[name];
-    }
-
-    function processType(type: GraphQLNamedType) {
-        if (type.name.substring(0, 2) === '__') {
-            // introspection types like __Schema do not need to be converted
-            return;
-        }
-        if (isNativeGraphQLType(type)) {
-            // don't rename native types. remapType also will not touch them.
-            return;
-        }
-        typeMap[type.name] = renameType(type, transformer(type.name), findType);
-    }
-
-    // Dependencies between fields and their are broken up via GraphQL's thunk approach (fields are only requested when
-    // needed, which is after all types have been converted). However, an object's reference to its implemented
-    // interfaces does not support the thunk approach, so we need to make sure they are converted first
-    originalTypes.filter(t => t instanceof GraphQLInterfaceType).forEach(processType);
-    originalTypes.filter(t => !(t instanceof GraphQLInterfaceType)).forEach(processType);
-
-    function findNewTypeMaybe(type: GraphQLObjectType|undefined) {
-        if (!type) {
-            return undefined;
-        }
-        const newType = findType(type.name);
-        return <GraphQLObjectType>newType;
-    }
-
-    return new GraphQLSchema({
-        types: Object.values(typeMap),
-        directives: schema.getDirectives(), // TODO rename
-        query: findNewTypeMaybe(schema.getQueryType())!,
-        mutation: findNewTypeMaybe(schema.getMutationType()),
-        subscription: findNewTypeMaybe(schema.getSubscriptionType()),
-    });
-}
-
-type TypeResolver = (name: string) => GraphQLNamedType;
-
-/**
- * Creates a new GraphQLType with a new name but otherwise the same configuration as the provided type. All named
- * types referenced within the type are sent through the typeResolver with their old name to determine the new type
- */
-function renameType(type: GraphQLNamedType, name: string, typeResolver: TypeResolver) {
-    if (type instanceof GraphQLScalarType) {
-        return new GraphQLScalarType({
-            name,
-            description: type.description,
-            serialize: type.serialize.bind(type),
-            parseLiteral: type.parseLiteral.bind(type),
-            parseValue: type.parseValue.bind(type)
-        });
-    }
-    if (type instanceof GraphQLObjectType) {
-        return renameObjectType(type, name, typeResolver);
-    }
-    if (type instanceof GraphQLInputObjectType) {
-        return renameInputObjectType(type, name, typeResolver);
-    }
-    if (type instanceof GraphQLInterfaceType) {
-        return renameInterfaceType(type, name, typeResolver);
-    }
-    if (type instanceof GraphQLUnionType) {
-        return new GraphQLUnionType({
-            name: name,
-            description: type.description,
-            types: type.getTypes().map(type => <GraphQLObjectType>typeResolver(type.name))
-        });
-    }
-    if (type instanceof GraphQLEnumType) {
-        return renameEnumType(type, name, typeResolver);
-    }
-    throw new Error(`Unsupported type: ${type}`);
-}
-
-function renameObjectType(type: GraphQLObjectType, name: string, typeResolver: TypeResolver) {
-    return new GraphQLObjectType({
-        name,
-        description: type.description,
-        fields: () => remapFieldTypes(type.getFields(), typeResolver),
-        interfaces: type.getInterfaces().map(iface => <GraphQLInterfaceType>typeResolver(iface.name))
-    });
-}
-
-function renameInterfaceType(type: GraphQLInterfaceType, name: string, typeResolver: TypeResolver) {
-    return new GraphQLInterfaceType({
-        name,
-        description: type.description,
-        fields: () => remapFieldTypes(type.getFields(), typeResolver),
-
-        // this is likely not to work, so it should be overwritten later, but it's our best choice. Leaving it null
-        // will cause the schema invariants to fail (either resolveType or isTypeOf needs to be implemented)
-        resolveType: !type.resolveType ? undefined :
-            (value: any,context: any, info: GraphQLResolveInfo) => type.resolveType(value, context, info)
-    });
+export function renameTypes(schema: GraphQLSchema, typeNameTransformer: (typeName: string) => string): GraphQLSchema {
+    return transformSchema(schema, new RenamingTransformer(typeNameTransformer));
 }
 
 /**
- * Creates field configs for all provided fields, but with remapped types and argument types. All named
- * types are sent through the typeResolver with their old name to determine the new type.
+ * A schema transformer that renames all no-native types according to a simple provided function
  */
-function remapFieldTypes(originalFields: GraphQLFieldMap<any, any>, typeResolver: TypeResolver): GraphQLFieldConfigMap<any, any> {
-    const fields: GraphQLFieldConfigMap<any, any> = {};
-    for (const fieldName in originalFields) {
-        const originalField = originalFields[fieldName];
-        const args: GraphQLFieldConfigArgumentMap = {};
-        for (const arg of originalField.args) {
-            args[arg.name] = {
-                description: arg.description,
-                type: <GraphQLInputType>remapType(arg.type, typeResolver),
-                defaultValue: arg.defaultValue
-            };
-        }
-        fields[fieldName] = {
-            description: originalField.description,
-            deprecationReason: originalField.deprecationReason,
-            type: <GraphQLOutputType>remapType(originalField.type, typeResolver),
-            args: args
-        };
-    }
-    return fields;
-}
-
-function renameInputObjectType(type: GraphQLInputObjectType, name: string, typeResolver: TypeResolver) {
-    const originalFields = type.getFields();
-    function getFields() {
-        const fields: GraphQLInputFieldConfigMap = {};
-        for (const fieldName in originalFields) {
-            const originalField = originalFields[fieldName];
-            fields[fieldName] = {
-                description: originalField.description,
-                defaultValue: originalField.defaultValue,
-                type: <GraphQLInputType>remapType(originalField.type, typeResolver)
-            };
-        }
-        return fields;
+class RenamingTransformer implements SchemaTransformer {
+    constructor(private typeNameTransformer: (typeName: string) => string) {
     }
 
-    return new GraphQLInputObjectType({
-        name,
-        description: type.description,
-        fields: getFields
-    });
-}
+    transformEnumType = this.rename;
+    transformInterfaceType = this.rename;
+    transformObjectType = this.rename;
+    transformScalarType = this.rename;
+    transformUnionType = this.rename;
+    transformInputObjectType = this.rename;
 
-function renameEnumType(type: GraphQLEnumType, name: string, typeResolver: TypeResolver) {
-    const values: GraphQLEnumValueConfigMap = {};
-    for (const originalValue of type.getValues()) {
-        values[originalValue.name] = {
-            description: originalValue.description,
-            value: originalValue.value,
-            deprecationReason: originalValue.deprecationReason
-        };
+    private rename(config: { name: string }) {
+        config.name = this.typeNameTransformer(config.name);
     }
-
-    return new GraphQLEnumType({
-        name,
-        description: type.description,
-        values
-    });
-}
-
-/**
- * Gets a GraphQLType of the same structure as type, but with the underlying named type replaced by the one provided
- * by namedTypeResolver for the same name
- */
-function remapType(type: GraphQLType, namedTypeResolver: TypeResolver): GraphQLType {
-    if (type instanceof GraphQLList) {
-        return new GraphQLList(remapType(type.ofType, namedTypeResolver));
-    }
-    if (type instanceof GraphQLNonNull) {
-        return new GraphQLNonNull(remapType(type.ofType, namedTypeResolver));
-    }
-    if (isNativeGraphQLType(type)) {
-        // do not rename native types but keep the reference to singleton objects like GraphQLString
-        return type;
-    }
-    return namedTypeResolver(type.name);
 }
