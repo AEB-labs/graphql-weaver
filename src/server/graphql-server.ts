a26af904fef42d2ee78ffff8502a6ef38b75b01b
Sebastian Henke <s.henke@henke-informatik.de>
2017-07-10 22:23:04 +0200

    Make graphql server stoppable.

--- src/server/graphql-server.ts
+++ src/server/graphql-server.ts
@@ -4,6 +4,7 @@ import * as cors from 'cors';
 import {graphqlExpress, graphiqlExpress} from "graphql-server-express";
 import {SchemaProvider} from "./schema-provider";
 import {GraphQLOptions} from 'graphql-server-core';
+import {Server} from "http";
 
 export interface GraphQLServerConfig {
     readonly port: number;
@@ -12,14 +13,25 @@ export interface GraphQLServerConfig {
 
 
 export class GraphQLServer {
+
+    private server: Server;
+
     constructor(private readonly config: GraphQLServerConfig) {
         const app = express();
         app.use(cors());
         app.get('/', (req, res) => { res.redirect('/graphiql')});
         app.use('/graphql', bodyParser.json(), graphqlExpress(() => this.getGraphQLOptions()));
         app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+        this.server = app.listen(config.port, () => {
+            console.log(`GraphQL server started on port ${config.port}.`);
+        });
+    }
 
-        app.listen(config.port);
+    public stop() {
+        if (this.server) {
+            this.server.close();
+            console.log('GraphQL server stopped.');
+        }
     }
 
     private getGraphQLOptions(): GraphQLOptions {
