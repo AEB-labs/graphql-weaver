1a05b32411889beeb8e809d93628c7c01210febd
Sebastian Henke <s.henke@henke-informatik.de>
2017-07-11 10:51:47 +0200

    Integration tests for nested fragments on linked schemas.

---
+++ spec/regression/data/nested_fragments_on_links.config.ts
@@ -0,0 +1,47 @@
+import {GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString} from 'graphql';
+import * as path from 'path';
+import {loadProxyConfig} from '../../../src/config/load-config';
+import {ProxyConfig} from "../../../src/config/proxy-configuration";
+import {testTypes} from "../../helpers/test-types";
+
+export async function getConfig() {
+    return <ProxyConfig>{
+        endpoints: [
+            {
+                namespace: 'staticData',
+                url: "http://localhost:1337/graphql",
+                typePrefix: 'CountryNS'
+            },
+            {
+                schema: new GraphQLSchema({
+                    query: new GraphQLObjectType({
+                        name: 'Query',
+                        fields: {
+                            horst: {
+                                type: testTypes.personType,
+                                resolve: () => ({nationality: 'DE', name: "Horst"})
+                            },
+                            allPeople: {
+                                type: new GraphQLList(testTypes.personType),
+                                resolve: () => ([
+                                    {nationality: 'DE', name: "Horst"},
+                                    {nationality: 'DE', name: "Helga"},
+                                    {nationality: 'FR', name: "Jacqueline"}])
+                            }
+                        }
+                    })
+                }),
+                fieldMetadata: {
+                    'Person.nationality': {
+                        link: {
+                            field: "staticData.Country",
+                            argument: "identCode",
+                            batchMode: false
+                        }
+                    },
+                }
+            }
+        ],
+        port: 3100
+    };
+}
