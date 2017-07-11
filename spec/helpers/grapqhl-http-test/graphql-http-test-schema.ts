b301520f401db73c379f7e2252e71661e0a792e1
Jan Melcher <jan.melcher@aeb.com>
2017-07-11 17:01:59 +0200

    Add people to static data endpoint for regression tests

--- spec/helpers/grapqhl-http-test/graphql-http-test-schema.ts
+++ spec/helpers/grapqhl-http-test/graphql-http-test-schema.ts
@@ -1,7 +1,10 @@
-import {GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString, valueFromAST} from "graphql";
+import { GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import * as fs from 'fs-extra';
-import * as path from "path";
-import {testTypes} from "../test-types";
+import * as path from 'path';
+import { testTypes } from '../test-types';
+
+const allCountries = getAllCountries();
+const allPeople = getAllPeople();
 
 export const defaultTestSchema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -11,42 +14,65 @@ export const defaultTestSchema = new GraphQLSchema({
                 type: new GraphQLList(testTypes.countryType),
                 resolve: async (obj, args) => {
                     if (args.filter && args.filter.identCode_in) {
-                        return (await allCountries()).filter(country => args.filter.identCode_in.includes(country.identCode))
+                        return (await allCountries).filter(country => args.filter.identCode_in.includes(country.identCode));
                     }
-                    return await allCountries();
+                    return await allCountries;
                 },
                 args: {
-                    filter: { type: testTypes.countryFilterType },
+                    filter: {type: testTypes.countryFilterType}
                 }
             },
             Country: {
                 type: testTypes.countryType,
                 resolve: async (obj, args) => {
-                    if (args.id) return await objectById(args.id, await allCountries());
-                    if (args.identCode) return await countryByIdentCode(args.identCode);
+                    if (args.id) {
+                        return await objectById(args.id, await allCountries);
+                    }
+                    if (args.identCode) {
+                        return await countryByIdentCode(args.identCode);
+                    }
                     return undefined;
                 },
                 args: {
-                    identCode: { type: GraphQLString },
-                    id: { type: GraphQLString }
+                    identCode: {type: GraphQLString},
+                    id: {type: GraphQLString}
+                }
+            },
+            allPeople: {
+                type: new GraphQLList(testTypes.personType),
+                resolve: async (obj, args) => {
+                    let people = await allPeople;
+                    if (args.filter && args.filter.isCool != undefined) {
+                        people = people.filter(person => args.filter.isCool === person.isCool);
+                    }
+                    return await people;
+                },
+                args: {
+                    filter: {type: testTypes.personFilterType}
                 }
             }
         }
     })
 });
 
-function objectById(id: string, objects: [{id:string}]) {
+function objectById(id: string, objects: { id: string }[]) {
     return objects.filter(value => value.id == id)[0];
 }
 
 async function countryByIdentCode(identCode: string) {
-    return (await allCountries()).filter(value => value.identCode === identCode)[0];
+    return (await allCountries).filter(value => value.identCode === identCode)[0];
 }
 
-async function allCountries() { return <[{id:string,identCode:string,isoCode:string,description:string}]> await readTestDataFromJson('countries.json'); }
+async function getAllCountries() {
+    return <{ id: string, identCode: string, isoCode: string, description: string }[]> await readTestDataFromJson('countries.json');
+}
 // async function allDeliveries() { return await readTestDataFromJson('deliveries.json') }
+
+async function getAllPeople() {
+    return <{ name: string, nationality: string, isCool?: boolean }[]> await readTestDataFromJson('people.json');
+}
 
 async function readTestDataFromJson(filename: string) {
     const json: string = await fs.readFile(path.resolve(__dirname, filename), 'utf-8');
-    return <[{id:string}]> JSON.parse(json).data;
+    return JSON.parse(json).data;
 }
--- spec/helpers/test-types.ts
+++ spec/helpers/test-types.ts
@@ -1,7 +1,9 @@
 import {
-    GraphQLBoolean, GraphQLID, GraphQLInputObjectType, GraphQLInputType, GraphQLInt, GraphQLList, GraphQLObjectType,
+    GraphQLBoolean, GraphQLEnumType, GraphQLID, GraphQLInputObjectType, GraphQLInputType, GraphQLInt, GraphQLList,
+    GraphQLObjectType,
     GraphQLString
-} from "graphql";
+} from 'graphql';
+import { arrayToObject, mapValues, objectFromKeys } from '../../src/utils/utils';
 
 export namespace testTypes {
     export const carType = new GraphQLObjectType({
@@ -21,13 +23,21 @@ export namespace testTypes {
         }
     });
 
+    const continents = ['Europe', 'NorthAmerica', 'SouthAmerica', 'Asia', 'Australia', 'Antarctica', 'Africa'];
+
+    export const continentType = new GraphQLEnumType({
+        name: 'Continent',
+        values: objectFromKeys(continents, key => ({}))
+    });
+
     export const countryType = new GraphQLObjectType({
         name: 'Country',
         fields: {
             id: { type: GraphQLID},
             identCode: { type: GraphQLString },
             isoCode: { type: GraphQLString },
-            description: { type: GraphQLString }
+            description: { type: GraphQLString },
+            continent: { type: continentType }
         }
     });
 
@@ -37,7 +47,16 @@ export namespace testTypes {
             identCode_in: { type: new GraphQLList(GraphQLString) }
         },
 
-    })
+    });
+
+
+    export const personFilterType = new GraphQLInputObjectType({
+        name: 'PersonFilter',
+        fields: {
+            isCool: { type: GraphQLBoolean }
+        },
+
+    });
 
 
 }
