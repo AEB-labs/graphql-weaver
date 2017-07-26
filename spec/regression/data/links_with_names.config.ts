844dded03a67d442709f8ae4b5f2c592a97057bb
Jan Melcher <jan.melcher@aeb.com>
2017-07-26 19:36:29 +0200

    Add tests for linkFieldName

---
+++ spec/regression/data/links_with_names.config.ts
@@ -0,0 +1,45 @@
+import { GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { ProxyConfig } from '../../../src/config/proxy-configuration';
+import { testTypes } from '../../helpers/test-types';
+
+export async function getConfig(): Promise<ProxyConfig> {
+    return {
+        endpoints: [
+            {
+                namespace: 'staticData',
+                url: "http://localhost:1337/graphql",
+                typePrefix: 'CountryNS'
+            },
+            {
+                schema: new GraphQLSchema({
+                    query: new GraphQLObjectType({
+                        name: 'Query',
+                        fields: {
+                            horst: {
+                                type: testTypes.personType,
+                                resolve: () => ({nationality: 'DE', name: "Horst"})
+                            },
+                            allPeople: {
+                                type: new GraphQLList(testTypes.personType),
+                                resolve: () => ([
+                                    {nationality: 'DE', name: "Horst"},
+                                    {nationality: 'DE', name: "Helga"},
+                                    {nationality: 'FR', name: "Jacqueline"}])
+                            }
+                        }
+                    })
+                }),
+                fieldMetadata: {
+                    'Person.nationality': {
+                        link: {
+                            field: "staticData.Country",
+                            argument: "identCode",
+                            batchMode: false,
+                            linkFieldName: 'country'
+                        }
+                    },
+                }
+            }
+        ]
+    };
+}
