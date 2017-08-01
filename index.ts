ee281d6f1ee3b52e994cb01489e7df78d4bb6288
Jan Melcher <jan.melcher@aeb.com>
2017-08-01 15:32:08 +0200

    Export toolbox to write modules

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
+// Main function
+export { createProxySchema } from './src/proxy-schema';
+
+// Configuration
 export { ProxyConfig, EndpointConfig } from './src/config/proxy-configuration';
+export {
+    PipelineConfig, PreMergeModuleContext, PostMergeModuleContext, PipelineModule
+} from './src/pipeline/pipeline-module';
+
+// GraphQL client
 export { GraphQLClient } from './src/graphql-client/graphql-client';
 export { HttpGraphQLClient } from './src/graphql-client/http-client';
 export { LocalGraphQLClient } from './src/graphql-client/local-client';
-export { createProxySchema } from './src/proxy-schema';
-export { FieldMetadata, LinkConfig, SchemaMetadata } from './src/extended-schema/extended-schema';
-export { EXTENDED_INTROSPECTION_FIELD, getExtendedIntrospectionType, getExtendedIntrospectionData }from './src/extended-schema/extended-introspection';
+
+// Extended schema
+export { ExtendedSchema, FieldMetadata, LinkConfig, SchemaMetadata } from './src/extended-schema/extended-schema';
+export {
+    EXTENDED_INTROSPECTION_FIELD, getExtendedIntrospectionType, getExtendedIntrospectionData
+} from './src/extended-schema/extended-introspection';
+
+// Utilities to write modules
+export {
+    ExtendedSchemaTransformer, transformExtendedSchema, GraphQLFieldConfigMapWithMetadata,
+    GraphQLFieldConfigWithMetadata, GraphQLNamedFieldConfigWithMetadata
+} from './src/extended-schema/extended-schema-transformer';
+export { Query } from './src/graphql/common';
+export {
+    SchemaTransformer, transformSchema, FieldsTransformationContext, DirectiveTransformationContext,
+    FieldTransformationContext, GraphQLNamedFieldConfig, GraphQLNamedInputFieldConfig, InputFieldTransformationContext,
+    TypeTransformationContext, SchemaTransformationContext
+} from './src/graphql/schema-transformer';
