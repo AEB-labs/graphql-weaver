fe8f2a035c3ef874a0e39e675189dfcf39e3cf56
Jan Melcher <jan.melcher@aeb.com>
2017-08-01 14:45:27 +0200

    Export GraphQLClient to configure ProxyConfig

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 export { ProxyConfig, EndpointConfig } from './src/config/proxy-configuration';
+export { GraphQLClient } from './src/graphql-client/graphql-client';
+export { HttpGraphQLClient } from './src/graphql-client/http-client';
+export { LocalGraphQLClient } from './src/graphql-client/local-client';
 export { createProxySchema } from './src/proxy-schema';
 export { FieldMetadata, LinkConfig, SchemaMetadata } from './src/extended-schema/extended-schema';
 export { EXTENDED_INTROSPECTION_FIELD, getExtendedIntrospectionType, getExtendedIntrospectionData }from './src/extended-schema/extended-introspection';
