a9201980fe33c8327f4522c92f2ba2e44237d75c
Jan Melcher <jan.melcher@aeb.com>
2017-10-22 18:28:40 +0200

    Use full ExecutionResult in regression result files
    This allows to test for errors

--- spec/regression/helpers.ts
+++ spec/regression/helpers.ts
@@ -1,7 +1,6 @@
-import { execute, graphql, parse, validate } from 'graphql';
-import {weaveSchemas} from "../../src/weave-schemas";
+import { ExecutionResult, graphql } from 'graphql';
+import { weaveSchemas } from '../../src/weave-schemas';
 import { WeavingConfig } from '../../src/config/weaving-config';
-import { assertSuccessfulResult } from '../../src/graphql/execution-result';
 
 /**
  * Creates a woven schema for a configuration and executes a query on it.
@@ -10,8 +9,7 @@ import { assertSuccessfulResult } from '../../src/graphql/execution-result';
  * @param variableValues
  * @returns {Promise<void>}
  */
-export async function testConfigWithQuery(proxyConfig: WeavingConfig, query: string, variableValues: {[name: string]: any}) {
+export async function testConfigWithQuery(proxyConfig: WeavingConfig, query: string, variableValues: {[name: string]: any}): Promise<ExecutionResult> {
     const schema = await weaveSchemas(proxyConfig);
-    const result = await graphql(schema, query, {cariedOnRootValue: true}, {}, variableValues, undefined);
-    return assertSuccessfulResult(result);
+    return graphql(schema, query, {cariedOnRootValue: true}, {}, variableValues, undefined);
 }
--- spec/regression/regressions.spec.ts
+++ spec/regression/regressions.spec.ts
@@ -26,7 +26,8 @@ describe('regression tests', () => {
         const queryString = fs.readFileSync(path.join(dir, fileName), 'utf-8');
         it(fileName, async() => {
             const configFile = require(path.join(dir, fileName.replace(/.graphql$/, '.config.ts')));
-            const expectedResult = JSON.parse(fs.readFileSync(path.join(dir, fileName.replace(/.graphql$/, '.result.json')), 'utf-8'));
+            let expectedResult = JSON.parse(fs.readFileSync(path.join(dir, fileName.replace(/.graphql$/, '.result.json')), 'utf-8'));
+            fs.writeFileSync(path.join(dir, fileName.replace(/.graphql$/, '.result.json')), JSON.stringify(expectedResult, undefined, '  '), 'utf-8');
             const variablesPath = path.join(dir, fileName.replace(/.graphql$/, '.vars.json'));
             const variableValues = fs.existsSync(variablesPath) ? JSON.parse(fs.readFileSync(variablesPath, 'utf-8')) : {};
             const result = await testConfigWithQuery(await configFile.getConfig(), queryString, variableValues);
