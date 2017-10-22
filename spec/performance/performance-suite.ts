a7978921304bb2b9ec3bd11be79c61648ce0b1fd
Jan Melcher <jan.melcher@aeb.com>
2017-10-23 01:18:59 +0200

    Automatically start test endpoint for benchmarks
    No need to bother the user...

--- spec/performance/performance-suite.ts
+++ spec/performance/performance-suite.ts
@@ -3,8 +3,9 @@ import { BenchmarkConfig, BenchmarkFactories } from './support/async-bench';
 import { QUERY_BENCHMARKS } from './profiling/query-pipeline.perf';
 import { COMPARISON } from './comparison/comparison';
 import { runComparisons } from './support/compare-runner';
+import { GraphQLHTTPTestEndpoint } from '../helpers/grapqhl-http-test/graphql-http-test-endpoint';
 
-console.log('Make sure graphql-weaver-test-schema is running');
+new GraphQLHTTPTestEndpoint().start(1337);
 
 const benchmarks: BenchmarkFactories = [
     ...QUERY_BENCHMARKS
