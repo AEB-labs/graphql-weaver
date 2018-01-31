35d90c5d5467fcde751c053f7970561a7f8835fb
Jan Melcher <jan.melcher@aeb.com>
2018-01-31 22:21:44 +0100

    Fix error when custom scalars are used as @link keys

---
+++ spec/regression/data/scalar-as-link-key.config.ts
@@ -0,0 +1,66 @@
+import { WeavingConfig } from '../../../src/config/weaving-config';
+import gql from 'graphql-tag';
+import { makeExecutableSchema } from 'graphql-tools';
+import { NonNegativeInt } from '../../helpers/non-negative-int';
+
+export async function getConfig(): Promise<WeavingConfig> {
+
+    const typeDefs = gql`
+        type Query {
+            locations(filter: LocationFilter): [Location]
+            events: [Event]
+        }
+        
+        type Event {
+            location: NonNegativeInt
+        }
+        
+        type Location {
+            id: NonNegativeInt
+        }
+        
+        input LocationFilter {
+            ids: [NonNegativeInt]
+        }
+        
+        scalar NonNegativeInt
+    `;
+
+    const schema = makeExecutableSchema({
+        typeDefs,
+        resolvers: {
+            NonNegativeInt,
+            Query: {
+                locations(source, args) {
+                    return args.filter.ids.map((id: number) => ({ id }));
+                },
+                events() {
+                    return [ { location: 0, }, { location: 1 }, { location: null }]
+                }
+            }
+        }
+    });
+
+    return {
+        endpoints: [
+            {
+                schema,
+                fieldMetadata: {
+                    'Event.location': {
+                        link: {
+                            field: 'locations',
+                            argument: 'filter.ids',
+                            batchMode: true,
+                            keyField: 'id'
+                        }
+                    },
+                    'Query.events': {
+                        join: {
+                            linkField: 'location'
+                        }
+                    }
+                }
+            }
+        ]
+    };
+}
