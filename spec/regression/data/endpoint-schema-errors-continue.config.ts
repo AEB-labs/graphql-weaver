cedc09bb82671afc8072c0ab6fe08918f2a5a764
Jan Melcher <jan.melcher@aeb.com>
2018-03-23 17:23:57 +0100

    Keep original fields if @link config fails
    Previously, this removed the field and might have caused an error if no fields were left in an object type.

--- spec/regression/data/endpoint-schema-errors-continue.config.ts
+++ spec/regression/data/endpoint-schema-errors-continue.config.ts
@@ -3,12 +3,25 @@ import { GraphQLClient } from '../../../src/graphql-client/graphql-client';
 import { ExecutionResult } from 'graphql';
 import { defaultTestSchema } from '../../helpers/grapqhl-http-test/graphql-http-test-schema';
 import { WeavingErrorHandlingMode } from '../../../src/config/error-handling';
+import gql from 'graphql-tag';
+import { makeExecutableSchema } from 'graphql-tools';
 
 const errorClient: GraphQLClient = {
     execute(query, vars, context, introspection): Promise<ExecutionResult> {
         throw new Error(introspection ? 'Throwing introspection' : 'Throwing query');
     }
 };
+
+const normalSchema = makeExecutableSchema({
+    typeDefs: gql`type Query { field: String }`,
+    resolvers: {
+        Query: {
+            field() {
+                return 'hello';
+            }
+        }
+    }
+});
 
 export async function getConfig(): Promise<WeavingConfig> {
     return {
@@ -30,7 +43,21 @@ export async function getConfig(): Promise<WeavingConfig> {
                 namespace: 'working',
                 typePrefix: 'Working',
                 schema: defaultTestSchema
-            }
+            },
+            {
+                namespace: 'linkConfigError',
+                typePrefix: 'LinkConfigError',
+                schema: normalSchema,
+                fieldMetadata: {
+                    'Query.field': {
+                        link: {
+                            field: 'nonexisting',
+                            argument: 'id',
+                            batchMode: false
+                        }
+                    }
+                }
+            },
         ],
         errorHandling: WeavingErrorHandlingMode.CONTINUE
     };
