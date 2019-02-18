cfc942412785c3887b738da8fd5fbd256cc17c76
Jan Melcher <jan.melcher@aeb.com>
2019-02-18 17:32:41 +0100

    Pass through operation names to link targets

--- spec/regression/data/operation-name.config.ts
+++ spec/regression/data/operation-name.config.ts
@@ -2,6 +2,16 @@ import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import { WeavingConfig } from '../../../src/config/weaving-config';
 
 export async function getConfig(): Promise<WeavingConfig> {
+    const linkTargetType = new GraphQLObjectType({
+        name: 'OpNameObject',
+        fields: {
+            opName: {
+                type: GraphQLString,
+                resolve: (source, args, context, info) => info.operation.name ? info.operation.name.value : null
+            },
+        }
+    });
+
     return {
         endpoints: [
             {
@@ -12,10 +22,35 @@ export async function getConfig(): Promise<WeavingConfig> {
                             opName: {
                                 type: GraphQLString,
                                 resolve: (source, args, context, info) => info.operation.name ? info.operation.name.value : null
+                            },
+
+                            linkTarget: {
+                                type: linkTargetType,
+                                args: {
+                                    // just to please link module
+                                    id: {
+                                        type: GraphQLString
+                                    }
+                                },
+                                resolve: () => ({})
+                            },
+
+                            link: {
+                                type: GraphQLString,
+                                resolve: () => 'dummy'
                             }
                         }
                     })
-                })
+                }),
+                fieldMetadata: {
+                    'Query.link': {
+                        link: {
+                            field: 'linkTarget',
+                            argument: 'id',
+                            batchMode: false
+                        }
+                    }
+                }
             }
         ]
     };
